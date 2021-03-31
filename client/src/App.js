0ff0c6c7691a39f2528f3c35c779ba29b1f2c6c2
Curious <curious.startpro@gmail.com>
2021-03-31 22:57:00 +0500

    Updated: style of components

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
-import NewTodo from "./components/NewTodo";
+import NewTodo from "./components/new-todo";
 import Main from "./components/main";
 import Alert from "./components/alert";
 import "./App.css";
 
-const App = () => (
-  <div className="container">
-    <h1 className="title">Todo App</h1>
-    <div className="main">
-      <NewTodo />
-      <Alert />
-      <Main />
+const App = () => {
+
+  return (
+    <div className="container">
+      <h1 className="title">Todo App</h1>
+      <div className="main">
+        <NewTodo />
+        <Alert />
+        <Main />
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default App;
