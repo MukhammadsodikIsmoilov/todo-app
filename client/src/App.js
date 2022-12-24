baba0ec9f530baa9631ff972a2fc098ace903312
curious-33 <curious.startpro@gmail.com>
2022-12-24 18:58:01 +0500

    refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,19 @@
 import React from "react";
 import NewTodo from "./components/new-todo";
 import Main from "./components/main";
-import Alert from "./components/alert";
+import Alert from "./components/Alert/Alert.jsx";
+import Header from "./components/Header/Header.jsx";
 import "./App.css";
 
-const App = () => {
-
-  return (
-    <div className="container">
-      <h1 className="title">Todo App</h1>
-      <div className="main">
-        <NewTodo />
-        <Alert />
-        <Main />
-      </div>
-    </div>
-  );
-}
+const App = () => (
+	<div className='container'>
+		<Header title='Todo App' />
+		<div className='main'>
+			<NewTodo />
+			<Alert />
+			<Main />
+		</div>
+	</div>
+);
 
 export default App;
--- client/src/components/todos.jsx
+++ client/src/components/todos.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import TaskCounter from "./Counter/Counter";
 import Todo from "./todo";
 
 const TodoList = () => {
-  const todos = useSelector((state) => state.todoReducer.todos);
-  return (
-    <ul>
-      {todos.map((todo) => (
-        <Todo key={todo.id} todo={todo} />
-      ))}
-    </ul>
-  );
+	const todos = useSelector((state) => state.todoReducer.todos);
+	return (
+		<>
+			<ul>
+				{todos &&
+					todos.length > 0 &&
+					todos.map((todo) => <Todo key={todo.id} todo={todo} />)}
+			</ul>
+			{todos && todos.length > 0 && (
+				<TaskCounter
+					tasksLeft={todos.filter((todo) => !todo.is_completed).length}
+				/>
+			)}
+		</>
+	);
 };
 
 export default TodoList;
